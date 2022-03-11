bcf77c9e0be06e153c4106de6ace699a54bf18d1
Sharad8518 <sharadkum9@gmail.com>
2022-03-11 07:27:22 +0000

    first commit

---
+++ src/component/Sidemenu.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { IconName } from "react-icons/fa";
+
+
+function Sidemenu() {
+  return (
+    <div>
+    
+    <div className='logo_conten'>
+
+    <div className='logo'>
+
+    <div className=''></div>
+
+
+
+
+    </div>
+
+
+
+
+    </div>
+    
+
+
+
+
+
+
+    
+    
+    </div>
+  )
+}
+
+export default Sidemenu
